f754359b8177b8580e1687a048a798b4e674f6c6
iulia.florentina13 <iulia.florentina13@gmail.com>
2026-01-18 14:14:33 +0200

    eliminat de pe doua rute validateToken

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,15 +1,15 @@
-const express=require('express')
-const router=express.Router()
+const express = require('express')
+const router = express.Router()
 
-const eventsController=require("../controllers/events")
-const {validate} = require('../middlewares/validate')
+const eventsController = require("../controllers/events")
+const { validate } = require('../middlewares/validate')
 const { eventValidator } = require('../validators/eventsValidation')
 const { validateToken } = require('../middlewares/auth')
 
-router.post("/",validateToken, eventValidator,validate,eventsController.addEventController)
-router.delete("/:id",validateToken,eventsController.deleteEventController)
-router.put("/:id",validateToken,eventsController.updateEventController)
-router.get("/:id",validateToken,eventsController.getEventById)
-router.get("/",validateToken,eventsController.getEvents)
+router.post("/", validateToken, eventValidator, validate, eventsController.addEventController)
+router.delete("/:id", validateToken, eventsController.deleteEventController)
+router.put("/:id", validateToken, eventsController.updateEventController)
+router.get("/:id", eventsController.getEventById)
+router.get("/", eventsController.getEvents)
 
-module.exports=router
+module.exports = router
